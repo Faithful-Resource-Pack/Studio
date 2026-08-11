6312162232ef24e51cca62c739688aa3dcf3c746
Evorp <75297863+3vorp@users.noreply.github.com>
2026-08-10 21:36:59 -0700

    feat: improve 404 page styling
    use dark mode always, improve margins/padding, and fill page properly by accounting for the footer space

--- pages/404/index.vue
+++ pages/404/index.vue
@@ -1,20 +1,30 @@
 <template>
-	<div class="background-404 white--text text-center">
-		<h1 class="text-h4 pt-10">{{ $root.lang().missing_page.title }}</h1>
-		<p class="text-body-1 mb-0 mt-7">{{ $root.lang().missing_page.description }}</p>
-		<p class="text-body-1 mt-0">
-			<!-- horrible hack for hrefs -->
-			<span v-for="(str, i) in $root.lang().missing_page.main_page.split('%')" :key="str">
-				<router-link v-if="i % 2 !== 0" to="/dashboard">{{ str }}</router-link>
-				<template v-else>{{ str }}</template>
-			</span>
-		</p>
+	<div class="flex-grow-1 background-404">
+		<div style="height: 80px" />
+		<v-container class="text-center">
+			<h1 class="text-h4 my-4">{{ $root.lang().missing_page.title }}</h1>
+			<p class="text-body-1 text--secondary mb-0">{{ $root.lang().missing_page.description }}</p>
+			<p class="text-body-1 text--secondary">
+				<!-- horrible hack for hrefs -->
+				<span v-for="(str, i) in $root.lang().missing_page.main_page.split('%')" :key="str">
+					<router-link v-if="i % 2 !== 0" to="/dashboard">{{ str }}</router-link>
+					<template v-else>{{ str }}</template>
+				</span>
+			</p>
+		</v-container>
 	</div>
 </template>
 
 <script>
 export default {
 	name: "missing-page",
+	// bit of a hack but honestly I think this looks better than half light half dark
+	created() {
+		this.$vuetify.theme.dark = true;
+	},
+	destroyed() {
+		this.$vuetify.theme.dark = this.$root.theme.isDark;
+	},
 };
 </script>
 
@@ -22,9 +32,6 @@ export default {
 .background-404 {
 	background-image: url(https://faithfulpack.net/image/background/the_end.jpg);
 	background-position: center;
-	z-index: 1;
 	background-size: cover;
-	min-height: 100vh;
-	width: 100%;
 }
 </style>
